bd8646679c50145d93bb57021d8fc2a7b4cc7fc7
Vicki Niu <vicki.niu@gmail.com>
2018-07-20 17:01:31 +0300

    adding index, exercises

---
+++ exercises.js
@@ -0,0 +1,69 @@
+// Exercise #1: Print a 10 x 10 multiplication table
+// 0  1  2  3  4  5  6  7  8  9  10
+// 0  2  4  6  8  10 12 14 16 18 20
+// 0  3  6  9  12 15 18 21 24 27 30
+// ....
+
+void printMultiplicationTable() {
+
+}
+
+// Exercise #2: FizzBuzz!
+// List numbers from 1 to 100
+// On multiples of 3, print "Fizz"
+// On multiples of 5, print "Buzz"
+// On multiples of 3 and 5, print "FizzBuzz"
+// i.e.
+// 0 1 2 Fizz 4 Buzz Fizz 7 8 Fizz ...
+
+void FizzBuzz() {
+
+}
+
+// Exercise #3: Shopping Cart Register
+// Write a function that takes as input an object
+// with many items for a party, and outputs the
+// total sum of those items!
+
+void cashRegister(cart) {
+
+}
+
+// Input
+var cartForParty = {  
+  banana: "1.25",
+  handkerchief: ".99",
+  Tshirt: "25.01",
+  apple: "0.60",
+  nalgene: "10.34",
+  proteinShake: "22.36"
+};
+
+// Output
+cashRegister(cartForParty); // 60.55
+
+// Exercise #4: Reading List
+// Create an array of objects, where each object describes a book and has properties for the 
+// title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+// Iterate through the array of books. For each book, log the book title and book author 
+// like so: "The Hobbit by J.R.R. Tolkien".
+// Now use an if/else statement to change the output depending on whether you read it yet or not. 
+// If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, 
+// log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+// TODO: add more books!
+var myBooks = [{
+	title: "The Hobbit",
+	author: "J.R.R. Tolkien",
+	alreadyRead: true
+}, {
+	title: "Harry Potter and the Chamber of Secrets",
+	author: "J. K. Rowling",
+	alreadyRead: false
+}];
+
+
+void readingList(books) {
+
+}
+
--- index.js
+++ index.js
@@ -1,13 +1,178 @@
 var mongoose = require('mongoose');
 
 // Connecting to a local database!
-mongoose.connect('mongodb://localhost/mongoose_basics', function (err) {
+mongoose.connect('mongodb://localhost/mongoose_basics_1', function (err) {
 	if (err) throw err;
 	console.log('Successfully connected');
 });
 
 var authorSchema = mongoose.Schema({
-	firstName: String,
-	lastName: String
+	_id: mongoose.Schema.Types.ObjectId,
+	fullName: {
+		firstName: String,
+		lastName: String		
+	},
+	biography: String,
+	twitter: {
+		type: String,
+		// special type for validating our input data
+		validate: {
+			validator: function(text) {
+				// make sure text begins with 'http://www.twitter.com'
+				// or 'www.twitter.com'
+				return (text.indexOf('http://www.twitter.com') === 0 
+					|| text.indexOf('www.twitter.com') === 0
+					|| text.indexOf('https://www.twitter.com') === 0)
+			}
+		}
+	},
+	// twitter: String,
+	facebook: String,
+	instagram: String,
+	profilePicture: Buffer,
+	// the time when we created this entry
+	created: {
+		type: Date,
+		default: Date.now
+	},
 });
 
+// book: summary
+
+var bookSchema = mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	title: {
+		type: String,
+		unique: true,
+	},
+	summary: String,
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Author'
+	},
+	ratings: [
+	{
+		summary: String,
+		// set a  maximum and a minimum
+		numberOfStars: {
+			type: Number, 
+			min: 0, 
+			max: 5
+		},
+		created: {
+			type: Date,
+			default: Date.now
+		}
+	}],
+	published: Date,
+	picture: Buffer,
+	created: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Author = mongoose.model('Author', authorSchema);
+var Book = mongoose.model('Book', bookSchema);
+
+var jkRowling = new Author({
+	_id: new mongoose.Types.ObjectId(),
+	fullName: {
+		firstName: 'J. K.',
+		lastName: 'Rowling'
+	},
+	biography: 'J.K. Rowling is the author of the AMAZING harry potter series',
+	twitter: 'https://www.twitter.com/jkrowling'
+	// facebook, instagram, profile picture are not set!
+});
+
+jkRowling.save(function(err) {
+	if (err) throw err;
+
+	// if no error
+	console.log('Author successfully saved!');
+	console.log('ID' + jkRowling._id);
+
+	var harryPotter1 = new Book({
+		_id: new mongoose.Types.ObjectId(),
+		title: 'Harry Potter and the Deathly Hallows',
+		author: jkRowling._id,
+		ratings: [{
+			numberOfStars: 4.5
+		}, {
+			numberOfStars: 5,
+			summary: 'This was great!'
+		}]
+	});
+	harryPotter1.save(function(err) {
+		if (err) {
+			console.log('This book is a duplicate!');
+		}
+
+		console.log('Book successfully saved!!!');
+		console.log('yaaaaaaaaay');
+	});
+
+	var harryPotter2 = new Book({
+		_id: new mongoose.Types.ObjectId(),
+		author: jkRowling._id,
+		title: 'Harry Potter and the Chamber of Secrets',
+		ratings: [{
+			summary: 'I really enjoyed this book',
+			numberOfStars: 5
+		}, {
+			summary: 'It was just OK',
+			numberOfStars: 3
+		}, {
+			summary: 'Best book ever!!!!!',
+			numberOfStars: 5
+		}]
+	});
+	harryPotter2.save(function(err) {
+		if (err) {
+			console.log('This book is a duplicate!');
+		}
+		console.log('Book successfully saved!');
+	})
+});
+
+var johnGreen = new Author({
+	_id: new mongoose.Types.ObjectId(),
+	fullName: {
+		firstName: 'John',
+		lastName: 'Green'
+	},
+	biography: 'John Green is the author of Paper Towns and many other great books',
+	twitter: 'https://www.twitter.com/johngreen'
+});
+
+johnGreen.save(function(err) {
+	if (err) throw err;
+
+	console.log('Author successfully saved!');
+	var paperTowns = new Book({
+		_id: new mongoose.Types.ObjectId(),
+		author: johnGreen._id,
+		title: 'Paper Towns',
+		ratings: [{
+			summary: 'I really enjoyed this book',
+			numberOfStars: 5
+		}]
+	});
+
+	paperTowns.save(function(err) {
+		if(err) {
+			console.log('This book is a duplicate!');
+		}
+		console.log('Book successfully saved!');
+	})
+});
+
+// Let's write a search query!
+Book.find({
+	title: /Harry Potter/i
+}).exec(function(err, books) {
+	if (err) throw err;
+	// TODO: calculate and print the average rating for each book!
+	console.log(books);
+});
